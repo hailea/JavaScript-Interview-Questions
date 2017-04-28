e83cf19d5e99717b2ddbea6b90fa499d8301babb
Haileab <haileab.abrha@outlook.com>
2017-04-28 12:56:44 -0400

    Test your front end developer skill
    Part I which includes javascript html5 and css3

--- interview.js
+++ interview.js
@@ -1,7 +1,13 @@
-$(document).ready(function () {
+﻿$(document).ready(function () {
+    var count = 0;
     $(".bigWord").on("click", function () {
+        count++;
+        if (count % 2 == 0) {
+            $(".defn").removeClass("myReply");
+        }else{
         $(".defn").removeClass("myReply");
         $(this).children().addClass("myReply");
+        }
     });
     $(".bigWord").on("dblclick", function () {
         $(".defn").removeClass("myReply");
